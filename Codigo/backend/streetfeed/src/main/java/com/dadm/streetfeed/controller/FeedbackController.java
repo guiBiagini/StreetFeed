be6a5f52c8da5e6daa73f1485aa1a7f98ac13262
Bruno Silveira Cequeira Lima <brunobhsclima@gmail.com>
2022-06-06 16:17:52 -0300

    Update FeedbackController.java

--- Codigo/backend/streetfeed/src/main/java/com/dadm/streetfeed/controller/FeedbackController.java
+++ Codigo/backend/streetfeed/src/main/java/com/dadm/streetfeed/controller/FeedbackController.java
@@ -1,11 +1,34 @@
 package com.dadm.streetfeed.controller;
 
+import com.dadm.streetfeed.domain.Feedback;
 import com.dadm.streetfeed.service.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class FeedbackController {
     @Autowired
     private FeedbackService service;
+
+    @RequestMapping(method = RequestMethod.POST, value = "cadFb")
+    public ResponseEntity<Feedback> cadastra(@RequestBody Feedback fb){
+        return ResponseEntity.ok(service.create(fb));
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "listFb")
+    public ResponseEntity<List<Feedback>> lista(){
+        return ResponseEntity.ok(service.listFeedbacks());
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "listFbUser")
+    public ResponseEntity<List<Feedback>> listaByUser(@RequestParam long id){
+        return ResponseEntity.ok(service.listFeedbacksByUsuario(id));
+    }
 }
